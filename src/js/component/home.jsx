dc57ca313b782993ae62e5209d9be68689a88ea5
Carlos  Castellanos <carcas4444@gmail.com>
2018-06-09 00:59:20 +0000

    third pudh

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,42 +2,64 @@ import React from 'react';
 
 //include images into your bundle
 import rigoImage from '../../img/rigo-baby.jpg';
-
+import {Player} from './player.jsx';
 //create your first component
 export class Home extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            songs:[
+                {title:"South Park", id:"south-park", author:"Kyle", url:"https://assets.breatheco.de/apis/sound/sounds//mario/songs/castle.mp3"},
+                {title:"Thunder Cats", id:"thundercats", author:"Moonra", url:"https://assets.breatheco.de/apis/sound/sounds/videogame/fx_zelda_recorder.wav"},
+                {title:"X-Men", id:"x-men", author:"Profesor", url:"https://assets.breatheco.de/apis/sound/sounds/cartoons/songs/x-men.mp3"}
+            ]
+        };
+        this.player=null;
+    }
+    playPlayer(){
+        
+    }
+    pausePlayer(){
+        
+    }
     
     render(){
         return (
-            <table className="table table-dark">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div>
+                <div className="text-center">
+                    <i className="fas fa-backward text-white pr-3"></i>
+                    <i className="fas fa-play text-white pr-3"></i>
+                    <i className="fas fa-forward text-white pr-3"></i>
+                    <audio ref={(t) => this.player = t} controls/>
+                </div>
+                <table className="table table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                        
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            
+                        </tr>
+                        
+                    </tbody>
+                </table>
+                
+            </div>
         );
     }
 }
---
+++ src/js/component/icons.js
@@ -0,0 +1,34 @@
+/* global FontAwesomeConfig */
+
+/**
+ * 1) First you have to import the @fortawesome/fontawesome library
+*/
+import fontawesome from '@fortawesome/fontawesome';
+
+
+/**
+ * 2) Then you have to import every icon that you will use
+*/
+import faPlay from '@fortawesome/fontawesome-free-solid/faPlay';
+import faBackward from '@fortawesome/fontawesome-free-solid/faBackward';
+import faForward from '@fortawesome/fontawesome-free-solid/faForward';
+
+/**
+ * 3) Tell font-awesome that you want to replace your icons with SVGs (recomended for performance)
+*/
+fontawesome.config = {
+  autoReplaceSvg: 'nest'
+};
+
+/**
+ * 4) Add the icons into the font-awesome library
+*/
+fontawesome.library.add(
+    faPlay, faBackward,  faForward
+);
+
+
+/**
+ * 5) Now, you can place the icon tag anywhere you want to icon to show, for example:
+ *    <i class="fas fa-check"></i>
+*/
